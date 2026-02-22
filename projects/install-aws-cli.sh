d0cebcae0a79258628ee2ab74798990a9d62958d
Ubuntu <ubuntu@ip-172-31-6-70.eu-north-1.compute.internal>
2026-02-22 21:01:55 +0000

    Created EC2 instance using shell script

--- projects/install-aws-cli.sh
+++ projects/install-aws-cli.sh
@@ -42,11 +42,86 @@ install_awscli(){
 fi
 	
 	unzip awscliv2.zip
-	sudo ./aws/install
+	 if command -v aws >/dev/null 2>&1; then
+        echo "AWS CLI already installed: $(aws --version)"
+        echo "Upgrading to latest version..."
+        sudo ./aws/install --update
+    else
+        echo "Installing AWS CLI..."
+        sudo ./aws/install
+    fi
+
 
 	aws --version
 	rm -rf awscliv2.zip ./aws
 }
 
-check_existing_aws || exit 1
-install_awscli || exit 1
+wait_for_instance() {
+    local instance_id="$1"
+    echo "Waiting for instance $instance_id to be in running state..."
+
+    while true; do
+        state=$(aws ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[0].Instances[0].State.Name' --output text)
+        if [[ "$state" == "running" ]]; then
+            echo "Instance $instance_id is now running."
+            break
+        fi
+        sleep 10
+    done
+}
+create_ec2_instance() {
+    local ami_id="$1"
+    local instance_type="$2"
+    local key_name="$3"
+    local subnet_id="$4"
+    local security_group_ids="$5"
+    local instance_name="$6"
+# Run AWS CLI command to create EC2 instance
+    instance_id=$(aws ec2 run-instances \
+        --image-id "$ami_id" \
+        --instance-type "$instance_type" \
+        --key-name "$key_name" \
+        --subnet-id "$subnet_id" \
+        --security-group-ids "$security_group_ids" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text
+    )
+ if [[ -z "$instance_id" ]]; then
+        echo "Failed to create EC2 instance." >&2
+        exit 1
+    fi
+
+    echo "Instance $instance_id created successfully."
+
+    # Wait for the instance to be in running state
+    wait_for_instance "$instance_id"
+}
+main() {
+	if command -v aws >/dev/null 2>&1; then
+        echo "AWS CLI already installed: $(aws --version)"
+        echo "Skipping installation."
+    else
+        install_awscli
+    fi
+
+
+    echo "Creating EC2 instance..."
+
+    # Specify the parameters for creating the EC2 instance
+    AMI_ID="ami-073130f74f5ffb161"
+    INSTANCE_TYPE="t3.micro"
+    KEY_NAME="SS_KEY"
+    SUBNET_ID="subnet-05e9cf2cec5418bf2"
+    SECURITY_GROUP_IDS="sg-084bf02e77d8bd3c5"  # Add your security group IDs separated by space
+    INSTANCE_NAME="Shell-Script-EC2-Demo"
+
+    # Call the function to create the EC2 instance
+    create_ec2_instance "$AMI_ID" "$INSTANCE_TYPE" "$KEY_NAME" "$SUBNET_ID" "$SECURITY_GROUP_IDS" "$INSTANCE_NAME"
+
+    echo "EC2 instance creation completed."
+}
+
+main "$@"
+
+
